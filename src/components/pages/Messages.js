7f30fe1c7586b2ff03b936bf2040e36b3d2f9b2f
Allen Wright <allenwrightkc@gmail.com>
2019-06-09 00:37:56 -0700

    fixes chat messages

--- src/components/pages/Messages.js
+++ src/components/pages/Messages.js
@@ -5,7 +5,7 @@ import { getChats } from '../../actions/chatActions';
 const Messages = ({ chats, getChats }) => {
   // chat sub-component
   const UserChat = ({ chat }) => {
-    const messages = chat.messages ? chat.messages.map(message => <ChatMessage message={message}/>) : null;
+    const messages = chat.messages ? chat.messages.map((message, idx) => <ChatMessage message={message} key={idx}/>) : null;
     return (
       <>
       {messages}
@@ -27,7 +27,7 @@ const Messages = ({ chats, getChats }) => {
   }, [getChats]);
 
   const { userChats } = chats;
-  const chatObjs = userChats ? userChats.map(chat => <UserChat chat={chat}/>) : null;
+  const chatObjs = userChats ? userChats.map((chat, idx) => <UserChat chat={chat} key={idx}/>) : null;
 
   return(
     <main className="messages">
